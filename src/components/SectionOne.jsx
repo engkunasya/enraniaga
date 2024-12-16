dfc01979099515050768adbbdcea1e0bdfe44ce2
nashnet1 <108474989+engkunasya@users.noreply.github.com>
2024-12-16 23:22:03 +0800

    yes

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -6,9 +6,12 @@ const SectionOne = () => {
       <div className="max-w-4xl p-6 text-center animate-fade-in">
         <h2 className="text-4xl font-bold mb-4 text-gray-800">Our Vision</h2>
         <p className="text-gray-600 leading-relaxed">
-          We aim to deliver stunning static websites with dynamic animation effects, combining 
-          simplicity and creativity to enhance user experience.
+        Our estate leads the way in producing high-quality, sustainably farmed palm oil, focusing on responsible practices and fresh, premium produce. Join us on our journey towards a greener future while offering our customers the best in agricultural innovation.
+<br/>
+
+
         </p>
+        
       </div>
     </div>
   );
